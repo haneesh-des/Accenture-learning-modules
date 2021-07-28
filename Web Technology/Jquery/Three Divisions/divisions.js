1cdb883a575b460823b6ed4c2bf0b86a4bdc4ef4
haneesh-des <62937406+haneesh-des@users.noreply.github.com>
2021-07-28 17:04:32 +0530

    Update divisions.js
    [name$='value'] finds string ends with give value

--- Web Technology/Jquery/Three Divisions/divisions.js
+++ Web Technology/Jquery/Three Divisions/divisions.js
@@ -1,5 +1,4 @@
 //WRITE YOUR jQUERY CODE HERE
 $('#button1').click(function(){
-    $("div[name$='DAIntelligence']").css( "background", "yellow" );
-    $("div[name$='ASI_Intelligence']").css( "background", "yellow" );
+    $("div[name$='Intelligence']").css( "background", "yellow" );
 });
